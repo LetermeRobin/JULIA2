8417d8d8ec4ca8fae7dbbc8e24849e3d0abac63c
LetermeRobin <60812419+LetermeRobin@users.noreply.github.com>
2024-07-14 21:33:08 +0200

    Update run_model.jl

--- src/run_model.jl
+++ src/run_model.jl
@@ -1,6 +1,9 @@
 using FSRU, Distances, JuMP
 
 include("data.jl")
+include("import.jl")
+include("export.jl")
+
 
 countries = ["AT", "BE", "BG", "CY", "CZ", "DE", "DK", "EE", "ES", "FI", "FR", "GR", "HR", "HU", "IE", "IT", "LT", "LU", "LV", "MT", "NL", "PL", "PT", "RO", "SE", "SI", "SK"]
 countries_lowercase = [lowercase(country) for country in countries]
