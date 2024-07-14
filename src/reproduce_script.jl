9500429f37f4086cd3f1adccddd9deba46e323d7
LetermeRobin <60812419+LetermeRobin@users.noreply.github.com>
2024-07-14 16:15:41 +0200

    Update reproduce_script.jl

--- src/reproduce_script.jl
+++ src/reproduce_script.jl
@@ -7,6 +7,3 @@ DEMAND = "Ours" # Change to "NZE" or "DE Gov" to test different scenarios
 BROWNFIELD = false
 include("run_model.jl")
 include("make_plots.jl")
-BROWNFIELD = true
-include("run_model.jl")
-include("make_plots.jl")
