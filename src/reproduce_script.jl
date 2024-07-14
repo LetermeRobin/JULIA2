529317be6cc9d5446f4e78eff6e45f35f72d086c
LetermeRobin <60812419+LetermeRobin@users.noreply.github.com>
2024-07-14 18:54:11 +0200

    Update reproduce_script.jl

--- src/reproduce_script.jl
+++ src/reproduce_script.jl
@@ -3,7 +3,5 @@ cd(joinpath(@__DIR__, ".."))
 Pkg.activate(".")
 Pkg.instantiate()
 
-DEMAND = "Ours" # Change to "NZE" or "DE Gov" to test different scenarios
 BROWNFIELD = false
 include("run_model.jl")
-include("make_plots.jl")
